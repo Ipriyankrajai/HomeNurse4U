1a4e1bcbb2fe44a7926e3d112374f826f342de0e
ayush137 <bhadouriaayush@gmail.com>
2024-05-20 08:24:33 +0530

    feat: Developed Feature Banner

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import HomePage from "../components/homePage/index";
+import FeaturesBanner from "@/components/common/FeaturesBanner";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`justify-between m-auto ${inter.className}`}
-    >
+    <main className={`justify-between m-auto ${inter.className}`}>
+      <FeaturesBanner />
       <HomePage />
     </main>
   );
