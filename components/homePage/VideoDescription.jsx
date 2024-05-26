d51888e4d8c05420fc1146ec8206cf52473dab3b
Priyank Rajai <priyank73@hotmail.com>
2024-05-26 19:06:44 +0530

    refactor: video description

--- components/homePage/VideoDescription.jsx
+++ components/homePage/VideoDescription.jsx
@@ -15,8 +15,8 @@ function VideoDescription() {
   }, [play]);
 
   return (
-    <div className="max-w-[1204px] px-[20px] mx-auto flex flex-wrap gap-x-[30px] gap-y-[15px]">
-      <div className="max-w-[330px]">
+    <div className="flex flex-wrap gap-x-[30px] gap-y-[15px] h-full">
+      <div className="max-w-[330px] calc-l-m">
         <div className="tb text-[#00856F]">
           VISITING NURSE FOR CLINICAL TRIALS
         </div>
@@ -35,7 +35,8 @@ function VideoDescription() {
           trials.
         </p>
       </div>
-      <div className="flex-1">
+
+      <div className="flex-1 relative calc-r-p self-start">
         <div>
           <div className="relative">
             <video ref={videRef} width={734} height={423}>
@@ -81,7 +82,7 @@ function VideoDescription() {
             </div>
           </div>
           <div className="border-l-2 border-[#D9D9D9] relative">
-            <div className="pt-5 pl-5 pb-[70px]">
+            <div className="pt-5 pl-5 pb-[30px]">
               <h3 className="h3">
                 HomeNurse4U makes the participation of patients in clinical
                 trials simpler and more comfortable reducing the burden on
@@ -94,36 +95,9 @@ function VideoDescription() {
                 the sites.
               </p>
             </div>
-            <div className="absolute bottom-[-50px]">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="902"
-                height="118"
-                viewBox="0 0 902 118"
-                fill="none"
-              >
-                <path
-                  d="M791.563 117.96H791.543C791.032 117.95 790.612 117.557 790.562 117.045L781.057 10.1216L769.13 85.1648C769.05 85.6576 768.63 86.0096 768.14 86.0096C767.649 86.0096 767.229 85.6375 767.159 85.1447L762.736 54.5723L758.774 77.4614C758.694 77.9441 758.274 78.2961 757.784 78.2961C757.293 78.2961 756.883 77.9441 756.803 77.4614L753.361 57.0764L749.549 63.6133C749.369 63.925 749.029 63.9799 748.679 63.9799H4.57168e-05L0 62.9799V61.9799H748.11L753.031 53.6772C753.241 53.3152 753.661 53.1241 754.072 53.1945C754.482 53.275 754.812 53.5968 754.882 54.0192L757.814 71.3569L761.856 48.0254C761.936 47.5426 762.356 47.1907 762.846 47.1907H762.856C763.347 47.1907 763.757 47.5628 763.837 48.0555L768.22 78.3363L780.386 1.82483C780.466 1.32199 780.907 0.970012 781.407 0.980068C781.907 1.00018 782.328 1.3924 782.368 1.89523L791.773 107.702L800.248 41.8003C800.308 41.2974 800.738 40.9253 801.238 40.9253C801.739 40.9253 802.169 41.3075 802.229 41.8103L806.671 78.0346L813.035 51.9676C813.145 51.505 813.575 51.1832 814.046 51.2033C814.516 51.2234 814.916 51.5653 814.996 52.038L818.048 70.12L821 60.4756C821.12 60.0934 821.45 59.8118 821.85 59.7716C822.24 59.7314 822.63 59.9325 822.831 60.2845L826.803 67.4851L830.155 62.5573C830.345 62.2858 830.67 61.9799 831 61.9799H1462V62.9799V63.9799H831.5L827.543 69.9691C827.343 70.2608 827.023 70.4116 826.673 70.4016C826.323 70.3814 826.012 70.1904 825.842 69.8887L822.23 63.3317L818.738 74.7662C818.598 75.2087 818.188 75.5104 817.718 75.4701C817.257 75.44 816.877 75.098 816.797 74.6354L813.845 57.1669L807.332 83.8373C807.212 84.31 806.791 84.6318 806.301 84.6016C805.821 84.5715 805.431 84.1994 805.371 83.7166L801.228 49.9663L792.593 117.105C792.533 117.608 792.103 117.98 791.603 117.98L791.563 117.96Z"
-                  fill="url(#paint0_linear_349_6851)"
-                />
-                <defs>
-                  <linearGradient
-                    id="paint0_linear_349_6851"
-                    x1="0.000122072"
-                    y1="59"
-                    x2="1462"
-                    y2="59"
-                    gradientUnits="userSpaceOnUse"
-                  >
-                    <stop stop-color="#D9D9D9" />
-                    <stop offset="0.615" stop-color="#00856F" />
-                    <stop offset="1" stop-color="#D9D9D9" />
-                  </linearGradient>
-                </defs>
-              </svg>
-            </div>
           </div>
         </div>
+        <div className="line-heart absolute bottom-0 w-full"></div>
       </div>
     </div>
   );
