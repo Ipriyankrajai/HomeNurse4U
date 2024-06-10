7a6f4b5814d8c3c4920735817fb3fc28a1edc885
Priyank Rajai <priyank73@hotmail.com>
2024-06-10 22:55:01 +0530

    Fix: banner text

--- components/homePage/banner.js
+++ components/homePage/banner.js
@@ -66,31 +66,30 @@ const Banner = (props) => {
             backgroundColor: "rgba(0, 0, 0, 0.5)",
           }}
         ></div>
-      </div>
-
-      <div className="max-w-[1084px] m-auto bg-[#ffffff]">
-        <div
-          style={{
-            height:
-              `${
-                banner?.backgroundMedia?.height -
-                (banner?.backgroundMedia?.height || 0) * 0.5
-              }px` || "400px",
-            marginTop:
-              `-${
-                banner?.backgroundMedia?.height -
-                (banner?.backgroundMedia?.height || 0) * 0.5
-              }px` || "50px",
-          }}
-        >
-          <h1 className="ml-[20px] text-[64px] font-medium leading-[110%] text-white font-['Domaine_Display'] max-w-[700px]">
-            Providing visiting nurses for clinical trials to your patients
-          </h1>
-          <p className="ml-[20px] text-[18px] leading-[150%] font-normal text-white mt-[10px] max-w-[700px]">
-            An ISO 9001:2015 certified clinical trial company that provides the
-            best care and support to patients while staying where they feel most
-            comfortable, at their own home.
-          </p>
+        <div className="max-w-[1084px] m-auto absolute bottom-[50px] left-[30px]">
+          <div
+            style={{
+              height:
+                `${
+                  banner?.backgroundMedia?.height -
+                  (banner?.backgroundMedia?.height || 0) * 0.5
+                }px` || "400px",
+              marginTop:
+                `-${
+                  banner?.backgroundMedia?.height -
+                  (banner?.backgroundMedia?.height || 0) * 0.5
+                }px` || "50px",
+            }}
+          >
+            <h1 className="ml-[20px] text-[64px] font-medium leading-[110%] text-white font-['Domaine_Display'] max-w-[700px]">
+              Providing visiting nurses for clinical trials to your patients
+            </h1>
+            <p className="ml-[20px] text-[18px] leading-[150%] font-normal text-white mt-[10px] max-w-[700px]">
+              An ISO 9001:2015 certified clinical trial company that provides
+              the best care and support to patients while staying where they
+              feel most comfortable, at their own home.
+            </p>
+          </div>
         </div>
       </div>
     </div>
